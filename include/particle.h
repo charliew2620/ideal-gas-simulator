3b4173786e6118119f03c76bbc4dc0efce7b9ca6
charliew2620 <cywang3@illinois.edu>
2021-03-13 00:07:27 -0600

    Initializes Particle methods and variables.

--- include/particle.h
+++ include/particle.h
@@ -7,11 +7,27 @@ namespace idealgas {
 
 class Particle {
 
-
  public:
 
-  const char* particle_color = "orange";
+  const std::string particle_color = "orange";
   const float particle_radius = 10;
 
+  Particle(const glm::vec2& position, const glm::vec2& velocity, const std::string color, const double radius);
+
+  void Draw();
+  void UpdateParticle();
+
+  const glm::vec2 &GetPosition() const;
+  const glm::vec2 &GetVelocity() const;
+  const std::string &GetColor() const;
+  const double &GetRadius() const;
+
+ private:
+  glm::vec2  position_;
+  glm::vec2  velocity_;
+  std::string color_;
+  double radius_;
+
+
 };
-}
+} // namespace idealgas
--- src/particle.cc
+++ src/particle.cc
@@ -5,4 +5,29 @@
 
 namespace idealgas {
 
+Particle::Particle(const vec2& position, const vec2& velocity, const std::string color,
+                   const double radius) {
+  position_ = position;
+  velocity_ = velocity;
+  color_ = color;
+  radius_ = radius;
+}
+
+const glm::vec2& Particle::GetPosition() const {
+  return position_;
+}
+const glm::vec2& Particle::GetVelocity() const {
+  return velocity_;
+}
+const std::string& Particle::GetColor() const {
+  return color_;
+}
+const double& Particle::GetRadius() const {
+  return radius_;
+}
+void Particle::UpdateParticle() {
+}
+void Particle::Draw() {
+}
+
 }
